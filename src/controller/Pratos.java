14e51c7d77966ed20a4be4c78a89a731c651b2df
gabbi <gabbi@LAPTOP-2HS469Q4>
2021-03-17 19:06:02 -0300

    Correção para que não ocorra repetição do número de Ids

--- src/controller/Pratos.java
+++ src/controller/Pratos.java
@@ -1,48 +1,48 @@
 package controller;
 
+import java.util.Set;
 import java.util.concurrent.Semaphore;
 
-public class Pratos extends Thread{
-	
-	private int id;
+public class Pratos extends Thread {
+
 	private Semaphore semaforo;
-	
-	public Pratos(int id, Semaphore semaforo) {
-	this.id = id;
-	this.semaforo = semaforo;
+
+	public Pratos(Semaphore semaforo) {
+		this.semaforo = semaforo;
 	}
 
 	@Override
 	public void run() {
 		try {
-			inicio(id);
+			inicio();
 			semaforo.acquire();
-			cozinhando(id);
+			cozinhando();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			semaforo.release();
 		}
-		
+
 	}
-	
-	private void inicio(int id) {
+
+	private void inicio() {
 		int tempo;
 		float percentualCozimento;
 			try {
-				if (id % 2 == 0) {
-				sleep(100);
-				tempo = (int)((Math.random() * 301 ) + 500);
-				percentualCozimento = (int)(tempo / 0.1f);
-				System.out.println("A " +id+ "a. sopa de cebola foi inicializada");
-				System.out.println("O percentual de cozimento da " +id+ "a. sopa de cebola foi " +percentualCozimento);
-			} else {
-				sleep(100);
-				tempo = (int)((Math.random() * 601 ) + 600);
-				percentualCozimento = (int)(tempo / 0.1f);
-				System.out.println("A " +id+ "a. lasanha à bolonhesa foi inicializada");
-				System.out.println("O percentual de da " +id+" a. lasanha à bolonhesa cozimento foi " +percentualCozimento);
-			}
+				if ((getId()) % 2 == 0) {
+					sleep(100);
+					tempo = (int) ((Math.random() * 301) + 500);
+					percentualCozimento = (int) (tempo / 0.1f);
+					System.out.println("A " + getId() + "a. sopa de cebola foi inicializada");
+					System.out.println("O percentual de cozimento da " + getId() + "a. sopa de cebola foi " + percentualCozimento);
+
+				} else {
+					sleep(100);
+					tempo = (int) ((Math.random() * 601) + 600);
+					percentualCozimento = (int) (tempo / 0.1f);
+					System.out.println("A " + getId() + "a. lasanha à bolonhesa foi inicializada");
+					System.out.println("O percentual de da " + (getId()) + " a. lasanha à bolonhesa cozimento foi "+ percentualCozimento);
+				}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -50,23 +50,22 @@ public class Pratos extends Thread{
 		
 	}
 
-	private void cozinhando(int id) {
+	private void cozinhando() {
 		int tempo;
 			try {
-				if (id % 2 == 0) {
+				if ((getId()) % 2 == 0) {
 				sleep(500);
 				tempo = (int)((Math.random() * 301 ) + 500);
-				System.out.println("A " +id+ "a. sopa de cebola está cozinhando");
+				System.out.println("A " +getId()+ "a. sopa de cebola está cozinhando");
 				
 			} else {
 				sleep(500);
 				tempo = (int)((Math.random() * 601) + 600);
-			}System.out.println("A " +id+ "a. lasanha à bolonhesa está pronta");
+			}System.out.println("A " +getId()+ "a. lasanha à bolonhesa está pronta");
 
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -10,8 +10,7 @@ public class Main {
 		int permissoes = 1;
 		Semaphore semaforo = new Semaphore(permissoes);
 		for (int i = 0; i < 5; i++) {
-			int id = (int)((Math.random()*11) + 1);
-			Thread t = new Pratos(id, semaforo);
+			Thread t = new Pratos(semaforo);
 			t.start();
 		}
 	}
